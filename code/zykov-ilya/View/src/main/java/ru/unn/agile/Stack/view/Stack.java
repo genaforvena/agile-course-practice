820c31110e0e0f6c0e87504da4085f7fc8c57f42
izykov <ilya.zykov@intel.com>
2015-11-25 03:59:06 +0300

    add View

---
+++ code/zykov-ilya/View/src/main/java/ru/unn/agile/Stack/view/Stack.java
@@ -0,0 +1,23 @@
+package ru.unn.agile.Stack.view;
+
+import javax.swing.*;
+
+public class Stack {
+    private JPanel mainPanel;
+    private JList list1;
+    private JButton buttonPop;
+    private JButton buttonPush;
+    private JTextField textFieldPush;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Stack");
+        frame.setContentPane(new Stack().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
