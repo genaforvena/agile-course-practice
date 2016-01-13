42a0d5f124229792ddaa98ed794c60dbe399ec28
Ilya Mozerov <imozerov@mera.ru>
2016-01-13 19:28:03 +0300

    Added implementation for ElasticityOfDemandCalculator.
    Added unittests.

--- code/mozerov-ilya/src/main/java/ru/unn/agile/ElasticityOfDemand/ElasticityOfDemandCalculator.java
+++ code/mozerov-ilya/src/main/java/ru/unn/agile/ElasticityOfDemand/ElasticityOfDemandCalculator.java
@@ -1,22 +1,37 @@
 package ru.unn.agile.ElasticityOfDemand;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * Created by imozerov on 12.01.16.
  */
 public class ElasticityOfDemandCalculator {
-    public double calculate(BigDecimal aPriceNew, BigDecimal aPriceOld,
-                      long aDemandQuantityNew, long aDemandQuantityOld) {
+    private static final int CHANGE_IN_PRICE_SCALE = 4;
+
+    public double calculate(final BigDecimal aPriceNew, final BigDecimal aPriceOld,
+                            final long aDemandQuantityNew, final long aDemandQuantityOld) {
+        if (!isBigDecimalValid(aPriceNew) || !isBigDecimalValid(aPriceOld)
+                || aDemandQuantityNew <= 0 || aDemandQuantityOld <= 0) {
+            throw new IllegalArgumentException(
+                    String.format("Invalid input! aPriceNew: %s, aPriceOld: %s, "
+                            + "aDemandQuantityNew: %d, aDemandQuantityOld: %d",
+                    aPriceNew, aPriceOld, aDemandQuantityNew, aDemandQuantityOld));
+        }
         return calculateChangeInDemand(aDemandQuantityNew, aDemandQuantityOld)
                 / calculateChangeInPrice(aPriceNew, aPriceOld);
     }
 
-    double calculateChangeInDemand(long aDemandQuantityNew, long aDemandQuantityOld) {
-        return 0;
+    double calculateChangeInDemand(final long aDemandQuantityNew, final long aDemandQuantityOld) {
+        return (double) (aDemandQuantityNew - aDemandQuantityOld) / aDemandQuantityOld;
     }
 
-    double calculateChangeInPrice(BigDecimal aPriceNew, BigDecimal aPriceOld) {
-        return 0;
+    double calculateChangeInPrice(final BigDecimal aPriceNew, final BigDecimal aPriceOld) {
+        return aPriceNew.subtract(aPriceOld)
+                .divide(aPriceOld, CHANGE_IN_PRICE_SCALE, RoundingMode.CEILING).doubleValue();
+    }
+
+    private boolean isBigDecimalValid(final BigDecimal aBigDecimal) {
+        return aBigDecimal != null && aBigDecimal.compareTo(BigDecimal.ZERO) > 0;
     }
 }
--- code/mozerov-ilya/src/test/java/ru/unn/agile/ElasticityOfDemand/ElasticityOfDemandCalculatorTest.java
+++ code/mozerov-ilya/src/test/java/ru/unn/agile/ElasticityOfDemand/ElasticityOfDemandCalculatorTest.java
@@ -12,32 +12,129 @@ import static org.junit.Assert.*;
 public class ElasticityOfDemandCalculatorTest {
 
     @Test
-    public void testCalculate_shouldReturnValidNumber() throws Exception {
+    public void calculateShouldReturnValidNumber() throws Exception {
         ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
-        assertEquals(0.111, calculator.calculate(new BigDecimal(10), new BigDecimal(9), 150, 110), 0.01);
+
+        double result = calculator.calculate(BigDecimal.TEN, new BigDecimal(9), 110, 150);
+
+        assertEquals(-2.39, result, 0.01);
     }
 
     @Test
-    public void testCalculate_sameNumbers_shouldZero() throws Exception {
+    public void calculateSameNumbersShouldReturnNan() throws Exception {
         ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
-        assertEquals(0, calculator.calculate(new BigDecimal(10), new BigDecimal(10), 150, 150), 0.01);
+
+        double result = calculator.calculate(BigDecimal.TEN, BigDecimal.TEN, 150, 150);
+
+        assertTrue(Double.isNaN(result));
+    }
+
+    @Test
+    public void calculateSamePriceAndDemandWasSmallerShouldReturnNegativeInfinity()
+            throws Exception {
+        ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
+
+        double result = calculator.calculate(BigDecimal.TEN, BigDecimal.TEN, 110, 150);
+
+        assertTrue(Double.isInfinite(result));
+        assertEquals(Double.NEGATIVE_INFINITY, result, 0.0001);
+    }
+
+    @Test
+    public void calculateSamePriceAndDemandWasBiggerShouldReturnNegativeInfinity()
+            throws Exception {
+        ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
+
+        double result = calculator.calculate(BigDecimal.TEN, BigDecimal.TEN, 150, 110);
+
+        assertTrue(Double.isInfinite(result));
+        assertEquals(Double.POSITIVE_INFINITY, result, 0.0001);
+    }
+
+    @Test
+    public void calculateSameDemandShouldReturnZero() throws Exception {
+        ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
+
+        double result = calculator.calculate(new BigDecimal(14), BigDecimal.TEN, 150, 150);
+
+        assertEquals(0, result, 0.00001);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testCalculate_shouldThrowOnInvalidInput() throws Exception {
+    public void calculateZeroPriceShouldThrow() throws Exception {
         ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
-        calculator.calculate(new BigDecimal(0), new BigDecimal(9), 150, 0);
+        calculator.calculate(BigDecimal.ZERO, new BigDecimal(9), 150, 4);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void calculateZeroDemandShouldThrow() throws Exception {
+        ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
+        calculator.calculate(new BigDecimal(16), new BigDecimal(9), 150, 0);
     }
 
     @Test
-    public void testCalculateChangeInDemand() throws Exception {
+    public void calculateChangeInDemandDemandWasBiggerShouldReturnNegativeValue()
+            throws Exception {
         ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
-        assertEquals(0.5, calculator.calculateChangeInDemand(1, 2), 0);
+
+        double result = calculator.calculateChangeInDemand(1L, 2L);
+
+        assertEquals(-0.5, result, 0);
     }
 
     @Test
-    public void testCalculateChangeInPrice() throws Exception {
+    public void calculateChangeInDemandDemandWasSmallerShouldReturnPositiveValue()
+            throws Exception {
         ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
-        assertEquals(0.5, calculator.calculateChangeInPrice(new BigDecimal(1), new BigDecimal(2)), 0);
+
+        double result = calculator.calculateChangeInDemand(3L, 2L);
+
+        assertEquals(0.5, result, 0);
+    }
+
+    @Test
+    public void calculateChangeInDemandDemandWasSmallerIntValuesShouldReturnPositiveValue()
+            throws Exception {
+        ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
+
+        double result = calculator.calculateChangeInDemand(3, 2);
+
+        assertEquals(0.5, result, 0);
+    }
+
+    @Test
+    public void calculateChangeInDemandSameNumbersShouldReturnZero() throws Exception {
+        ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
+
+        double result = calculator.calculateChangeInDemand(2L, 2L);
+
+        assertEquals(0, result, 0);
+    }
+
+    @Test
+    public void calculateChangeInPricePriceWasBiggerShouldReturnNegativeValue() throws Exception {
+        ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
+
+        double result = calculator.calculateChangeInPrice(BigDecimal.ONE, new BigDecimal(2));
+
+        assertEquals(-0.5, result, 0);
+    }
+
+    @Test
+    public void calculateChangeInPricePriceWasSmallerShouldReturnNegativeValue() throws Exception {
+        ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
+
+        double result = calculator.calculateChangeInPrice(new BigDecimal(3), new BigDecimal(2));
+
+        assertEquals(0.5, result, 0);
+    }
+
+    @Test
+    public void calculateChangeInPriceSameNumbersShouldReturnZero() throws Exception {
+        ElasticityOfDemandCalculator calculator = new ElasticityOfDemandCalculator();
+
+        double result = calculator.calculateChangeInPrice(BigDecimal.ONE, BigDecimal.ONE);
+
+        assertEquals(0, result, 0);
     }
 }
